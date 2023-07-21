73d80b17ae845e5a3e9322f1e032300c50a9f2ce
ssumrall <ssumrall@elon.edu>
2023-07-21 16:22:52 -0400

    Created repository interface

--- AbstractionOrganizer.Api/Models/IClassHeaderRepository.cs
+++ AbstractionOrganizer.Api/Models/IClassHeaderRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace AbstractionOrganizer.Api.Models
+﻿using AbstractionOrganizer.Models;
+
+namespace AbstractionOrganizer.Api.Models
 {
 	public interface IClassHeaderRepository
 	{
+		Task<IEnumerable<ClassHeader>> GetEmployees();
+
+		Task<ClassHeader> GetClassHeader(int classHeaderId);
+
+		Task<ClassHeader> AddEmployee(ClassHeader classHeader);
+
+		void DeleteEmployee(int classHeaderId);
+
+
 	}
 }
