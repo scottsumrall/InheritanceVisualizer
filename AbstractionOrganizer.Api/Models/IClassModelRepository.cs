560456e4df6bcf974335f9872f37cfe41a639738
ssumrall <ssumrall@elon.edu>
2023-07-25 16:33:31 -0400

    Fixing up interfaces

---
+++ AbstractionOrganizer.Api/Models/IClassModelRepository.cs
@@ -0,0 +1,17 @@
+﻿using AbstractionOrganizer.Models;
+
+namespace AbstractionOrganizer.Api.Models
+{
+	public interface IClassModelRepository
+	{
+		Task<IEnumerable<ClassModel>> GetClassModels();
+
+		Task<ClassModel> GetClassModel(int classModelId);
+
+		Task<ClassModel> AddClassModel(ClassModel classModel);
+
+		Task<ClassModel> DeleteClassModel(int classModelId);
+
+		Task<ClassModel> UpdateClassModel(ClassModel classModel);
+	}
+}
---
+++ AbstractionOrganizer.Api/Models/IMethodModelRepository.cs
@@ -0,0 +1,17 @@
+﻿using AbstractionOrganizer.Models;
+
+namespace AbstractionOrganizer.Api.Models
+{
+	public interface IMethodModelRepository
+	{
+		Task<IEnumerable<MethodModel>> GetMethodModels();
+
+		Task<MethodModel> GetMethodModel(int methodModelId);
+
+		Task<MethodModel> AddMethodModel(MethodModel methodModel);
+
+		Task<MethodModel> DeleteMethodModel(int methodModelId);
+
+		Task<MethodModel> UpdateMethodModel(MethodModel classModel);
+	}
+}
