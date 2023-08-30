8632b45c5b3065f575249603b9564f38c263bc9e
ssumrall <ssumrall@elon.edu>
2023-08-30 16:22:23 -0400

    Added model for methods

--- AbstractionOrganizer.Models/MethodModel.cs
+++ AbstractionOrganizer.Models/MethodModel.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AbstractionOrganizer.Models
 {
 	public class MethodModel
 	{
-	}
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public required int Id { get; set; }
+
+        [Required]
+        public required String Name { get; set; }
+
+        [Required]
+        public required AccessModifier AccessModifier { get; set; }
+
+        public required MethodModifier MethodModifier { get; set; }
+
+        public int ClassModelId { get; set; }
+
+        public bool GetsInherited { get; set; }
+
+        [JsonIgnore]
+        public ClassModel? ClassModel { get; set; } = null!;
+    }
+}
+
+public enum MethodModifier
+{
+    Defualt = 0,
+    New,
+    Virtual,
+    Abstract,
+    Sealed,
+    Static
 }
