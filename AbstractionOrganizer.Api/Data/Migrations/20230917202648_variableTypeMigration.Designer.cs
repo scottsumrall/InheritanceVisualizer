f08e7616c61d862aea07d38713847347bbaf61cd
ssumrall <ssumrall@elon.edu>
2023-09-17 16:34:28 -0400

    Migrations for addition of 'Type' field

--- AbstractionOrganizer.Api/Data/Migrations/20230917202648_variableTypeMigration.Designer.cs
+++ AbstractionOrganizer.Api/Data/Migrations/20230917202648_variableTypeMigration.Designer.cs
@@ -4,6 +4,7 @@ using AbstractionOrganizer.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AbstractionOrganizer.Api.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230917202648_variableTypeMigration")]
+    partial class variableTypeMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AbstractionOrganizer.Api/Data/Migrations/20230917202648_variableTypeMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbstractionOrganizer.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class variableTypeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "VariableModels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "VariableModels",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Type",
+                value: "Int");
+
+            migrationBuilder.UpdateData(
+                table: "VariableModels",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Type",
+                value: "String");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "VariableModels");
+        }
+    }
+}
